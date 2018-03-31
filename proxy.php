00ff9cc508f288261a4d60ebc7ab50abae189a26
Xiao Saiting <xsaiting@qq.com>
2018-03-31 11:51:03 +0800

    change api

--- proxy.php
+++ proxy.php
@@ -7,7 +7,7 @@ $api=[
     "liveOpen" => "https://plive.48.cn/livesystem/api/live/v1/openLivePage",
     "liveInfo" => "https://plive.48.cn/livesystem/api/live/v1/getLiveOne",
     "login"=> "https://puser.48.cn/usersystem/api/user/v1/login/phone",
-    "roomView" => "",
+    "roomId" => "https://pjuju.48.cn/imsystem/api/im/room/v1/login/user/list",
     "roomMain" => "https://pjuju.48.cn/imsystem/api/im/v1/member/room/message/mainpage",
     "roomBoard" => "https://pjuju.48.cn/imsystem/api/im/v1/member/room/message/boardpage",
 ];
