46a1cb4780b420bdb26efe7999c6953440aa0c98
Xiao Saiting <xsaiting@qq.com>
2018-10-09 09:28:29 +0800

    Update js/c.pocket48.page.js

--- js/c.pocket48.page.js
+++ js/c.pocket48.page.js
@@ -23,6 +23,8 @@ c.pocket48.page = c.pocket48.page || (function(){
         c.pocket48.page.token2();
         //更新token
         c.pocket48.page.updateToken();
+        //设置翻牌
+        c.pocket48.page.flipButton();
         //显示提示
         console.log('页面功能加载完毕');
         console.log("%c请勿分享Cookies给其他人"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em");
@@ -416,8 +418,8 @@ c.pocket48.page = c.pocket48.page || (function(){
     };
 
     //设置翻牌按钮 需要init
-    c.pocket48.page.flipBotton = function () {
-        $$(document).on('click', '.c-idolflip', function (e) {
+    c.pocket48.page.flipButton = function () {
+        $$(document).on('click', '.c-idolflip button', function (e) {
             //c-idolflip-123-456
             var id = $$(this).attr('id');
             var s = id.split('-');
