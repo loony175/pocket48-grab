d1364e8de539914f64a859d95bf338137557f36e
Xiao Saiting <xsaiting@qq.com>
2019-02-28 17:02:54 +0800

    v2.5.5 update
    1. fix team's color which is '#FFFFFF'.
    2. may be the last version using mdui.

--- js/c.pocket48.js
+++ js/c.pocket48.js
@@ -167,7 +167,7 @@ c.d = function(level){
 };
 
 //版本号
-c.pocket48.version='2.5.4.1';
+c.pocket48.version='2.5.5';
 //口袋48 api
 c.pocket48.api={
     sync: "https://psync.48.cn/syncsystem/api/cache/v1/update/overview",
